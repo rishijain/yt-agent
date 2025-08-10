3f45ac7f9514b8c52cad6526bd2cd7fe87c76b37
rishijain <jainrishi.37@gmail.com>
2025-08-10 23:32:21 +0530

    update the LLM call and promot

--- app/lib/agent/transcript.rb
+++ app/lib/agent/transcript.rb
@@ -8,8 +8,13 @@ class Agent::Transcript
     @llm.chat(message)
   end
 
-  def chat_with_prompt(prompt_type, content)
-    prompt = @prompt.send("#{prompt_type}_prompt", content)
+  def chat_with_prompt(prompt_type, content, options = {})
+    case prompt_type
+    when :analysis
+      prompt = @prompt.send("#{prompt_type}_prompt", content, options[:max_chapters])
+    else
+      prompt = @prompt.send("#{prompt_type}_prompt", content)
+    end
     @llm.chat(prompt)
   end
 end
